5f05a9086eb7a05946afdbc513fbb8d174432054
Augustin Pasquier <pasquieraugustin@outlook.fr>
2022-07-02 19:54:13 +0200

    delete members

--- index.js
+++ index.js
@@ -11,12 +11,11 @@ const data = require('./data.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // Parties du messages
-const CHANNEL = "967110593595179068" //"682859559680868356";
+const CHANNEL = "986235216479780884" //"682859559680868356";
 const MESSAGE_HEAD = "Hey @everyone !"
 const MESSAGE_BODY_1 = "fête ses"
 const MESSAGE_BODY_2 = "ans aujourd'hui ! Souhaitez-lui un excellent anniversaire ! Bon anniversaire"
 const MESSAGE_TAIL = "! :partying_face:"
-const MESSAGE_SERVER_TAIL = " ans que nous nous sommes réunis sur ce serveur ! Le temps passe si vite en votre compagnie ! Souvenirs, souvenirs, le premier message du serveur est --> [*cliquez sur le lien*] https://discord.com/channels/682859558934151181/682859559680868356/682859906935685120 :heart:";
 
 // Détermine si une date est passée
 function isPassed(date) {
@@ -61,17 +60,8 @@ client.on("ready", () => {
 				var date = new Date(data['members'][i]['year'], data['members'][i]['month'], data['members'][i]['day']);
 				var age = getAge(date);
 
-				// Anniversiare du serveur
-				if (data['members'][i]['name'] === "Le serveur") {
-					var message = `${MESSAGE_HEAD} Voilà maintenant ${age} ${MESSAGE_SERVER_TAIL}`;
-					client.channels.cache.get(CHANNEL).send(message);
-				}
-
-				// Anniversaire des membres
-				else {
-					var message = `${MESSAGE_HEAD} ${data['members'][i]['id']} ${MESSAGE_BODY_1} ${age} ${MESSAGE_BODY_2} ${data['members'][i]['name']} ${MESSAGE_TAIL}`;
-					client.channels.cache.get(CHANNEL).send(message);
-				};
+				var message = `${MESSAGE_HEAD} ${data['members'][i]['id']} ${MESSAGE_BODY_1} ${age} ${MESSAGE_BODY_2} ${data['members'][i]['name']} ${MESSAGE_TAIL}`;
+				client.channels.cache.get(CHANNEL).send(message);
 			};
 		};
 	});
@@ -93,10 +83,8 @@ client.on('interactionCreate', async interaction => {
 		};
 
 		var avg = 0;
-		for (let i = 2; i < membersAge.length; i++) {
-			avg += membersAge[i];
-		}
-		avg = avg / (membersAge.length - 2);
+		for (i in membersAge) {avg += membersAge[i]};
+		avg = avg / (membersAge.length);
 
 		const birthdays = new MessageEmbed()
 			.setColor("#DD2E44")
@@ -104,22 +92,12 @@ client.on('interactionCreate', async interaction => {
 			.setDescription("Voici tous les anniversaires à souhaiter :")
 			.setThumbnail("https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Twemoji12_1f973.svg/1200px-Twemoji12_1f973.svg.png")
 			.addFields(
-				{ name: "Le serveur", value: `28 février (${membersAge[0]} ans)`, inline: true },
-				{ name: "Notre mascotte Banga", value: `1er février (${membersAge[1]} ans)`, inline: true },
-				{ name: "\u200B", value: "\u200B", inline: true },
-				{ name: "Nico", value: `23 janvier (${membersAge[2]} ans)`, inline: true },
 				{ name: "Cho7Korn", value: `9 février (${membersAge[3]} ans)`, inline: true },
-				{ name: "Gavin's", value: `16 février (${membersAge[4]} ans)`, inline: true },
-				{ name: "Dam", value: `26 février (${membersAge[5]} ans)`, inline: true },
 				{ name: "Sainté", value: `3 avril (${membersAge[6]} ans)`, inline: true },
 				{ name: "Augustin", value: `29 avril (${membersAge[7]} ans)`, inline: true },
 				{ name: "Candice", value: `11 juin (${membersAge[8]} ans)`, inline: true },
-				{ name: "Mandy", value: `18 juin (${membersAge[9]} ans)`, inline: true },
-				{ name: "Chloé", value: `20 juillet (${membersAge[10]} ans)`, inline: true },
 				{ name: "Jérémy (Phénix)", value: `25 juillet (${membersAge[11]} ans)`, inline: true },
 				{ name: "Jérémy (Rellikorn)", value: `28 septembre (${membersAge[12]} ans)`, inline: true },
-				{ name: "Erwalia", value: `9 novembre (${membersAge[13]} ans)`, inline: true },
-				{ name: "Cindy", value: `13 décembre (${membersAge[14]} ans)`, inline: true },
 			)
 			.setTimestamp()
 			.setFooter({ text: `Moyenne d'âge du serveur : ${Math.round(avg)} ans`, iconURL: "https://cdn.discordapp.com/app-icons/775422653636149278/385a2dbe8bbfd559675a1bd43dbf4990.png" })
